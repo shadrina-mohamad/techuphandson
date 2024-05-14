dec69cced0b84ccd26af2dcbfbc6ac610584c73e
shadrina-mohamad <113326260+shadrina-mohamad@users.noreply.github.com>
2024-05-14 04:23:10 +0000

    Add js and button

---
+++ script.js
@@ -0,0 +1,38 @@
+var age = 20
+age = 65
+console.log(age)
+
+var username = "shadrina"
+var password = "techup"
+
+var isFemale = false
+var isLoggedIn = false
+
+var blog_posts = ["Today was a good day", "The sky is blue", 
+"It's raining heavily now"]
+
+console.log(username);
+console.log(password);
+console.log(isFemale);
+console.log(isLoggedIn);
+console.log(blog_posts);
+
+if (isFemale == true) {
+    console.log("You are female")
+}
+
+// conditionals
+if (isFemale == true) {
+    console.log("You are female")
+} else {
+    console.log("You are not female")
+}
+
+while (age < 70) {
+    console.log("Your age is " + age );
+    age = age + 1;
+}
+
+function sayHello() {
+    alert("Hello everyone");
+}
